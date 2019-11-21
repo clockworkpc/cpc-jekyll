4a6b35fbee24530d207829f7e8759f3b11b1bb95
Alexander Garber <clockworkpc@gmail.com>
2019-11-22 09:21:55 +1100

    Fresh gem

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in cpc-jekyll.gemspec
+gemspec
---
+++ spec/cpc/jekyll_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Cpc::Jekyll do
+  it "has a version number" do
+    expect(Cpc::Jekyll::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
